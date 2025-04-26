abd253cf5c885311c1c63b07fb32c44c3bd950a4
Alex Sinclaire <alexsinclaire1205@gmail.com>
2025-04-26 13:14:05 +0100

    Initial commit

---
+++ src/main/java/com/r3signed/ac/regions/ArdaRegions.java
@@ -0,0 +1,10 @@
+package com.r3signed.ac.regions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArdaRegions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/r3signed/ac/regions/client/ArdaRegionsClient.java
@@ -0,0 +1,10 @@
+package com.r3signed.ac.regions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArdaRegionsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
